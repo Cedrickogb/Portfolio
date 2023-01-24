1e2ce32142460401eae15af6cc5deeab1b9cafe2
Anonymll <cedrickogoubiyi13@gmail.com>
2023-01-25 00:20:36 +0100

    correction

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,8 +7,8 @@ export default function Navb() {
     const [open, setOpen] = useState(false);
     return (
         <div className="transition duration-700">
-            <FiMenu className="absolute sm:hidden block h-7 w-7 mr-2 mt-2 top-0 right-0 cursor-pointer bg-white " onClick={() => setOpen(!open)} />
-            <div className={`${open ? "block" : "hidden"} sm:flex flex-col py-2 xl:items-start fixed  h-full xl:ml-[96px] bg-green-900 overflow-auto scrollbar-hide `}>
+            <FiMenu className="absolute sm:hidden display h-7 w-7 mr-2 mt-2 top-0 right-0 cursor-pointer bg-white " onClick={() => setOpen(!open)} />
+            <div className={`${open ? "display" : "hidden"} sm:flex flex-col py-2 xl:items-start fixed  h-full xl:ml-[96px] bg-green-900 overflow-auto scrollbar-hide transition duration-700 translate-x-4 `}>
 
 
 
@@ -22,20 +22,20 @@ export default function Navb() {
                 </div>
 
                 <div className="mt-10 mb-20 xl:items-start">
-                    <nav className={`${open ? "block" : "hidden"}  sm:flex sm:items-center sm:w-auto w-auto `}>
+                    <nav className={`${open ? "display" : "hidden"}  sm:flex sm:items-center sm:w-auto w-auto `}>
 
                         <ul className="px-[3px] py-4 ">
                             <li className="font-semibold sm:px-[75px] py-5 text-white text-center hover:bg-slate-100 hover:text-slate-900 transition duration-700 border-t-2 ">
-                                <Link href="/conta#About me">Qui suis-je</Link>
+                                <Link href="#About me">Qui suis-je</Link>
                             </li>
                             <li className="font-semibold sm:px-[75px] py-5 text-white text-center hover:bg-slate-100 hover:text-slate-900 transition duration-700 border-t-2">
-                                <Link href="/conta#Points fort">Points fort</Link>
+                                <Link href="#Points fort">Points fort</Link>
                             </li>
                             <li className="font-semibold sm:px-[75px] py-5 text-white text-center hover:bg-slate-100 hover:text-slate-900 transition duration-700 border-t-2">
-                                <Link href="/conta#Projects">Projets</Link>
+                                <Link href="#Projects">Projets</Link>
                             </li>
                             <li className="font-semibold sm:px-[75px] py-5 text-white text-center hover:bg-slate-100 hover:text-slate-900 transition duration-700 border-t-2 border-b-2">
-                                <Link href="/conta#Contact">Contact</Link>
+                                <Link href="#Contact">Contact</Link>
                             </li>
 
                         </ul>
@@ -49,7 +49,7 @@ export default function Navb() {
                 </div>
 
                 <div className="px-10 pt-10 mt-20 xl:items-start sm:flex border-t-2 items-center">
-                    <p className="text-center text-white">Copyrigth 2022</p>
+                    <p className="text-center text-white">Copyrigth 2023</p>
                 </div>
 
             </div>
