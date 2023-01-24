84617dbcb17cab2d13c90ab2cae9b6aae9413f2b
Anonymll <cedrickogoubiyi13@gmail.com>
2023-01-25 00:55:03 +0100

    correction

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navb() {
     const [open, setOpen] = useState(false);
     return (
         <div className="transition duration-700">
-            <FiMenu className="absolute sm:hidden display h-7 w-7 mr-2 mt-2 top-0 right-0 cursor-pointer bg-white " onClick={() => setOpen(!open)} />
+            <FiMenu className="fixed sm:hidden display h-7 w-7 mr-2 mt-2 top-0 right-0 cursor-pointer bg-white " onClick={() => setOpen(!open)} />
             <div className={`${open ? "display" : "hidden"} sm:flex flex-col py-2 xl:items-start fixed  h-full xl:ml-[96px] bg-green-900 overflow-auto scrollbar-hide transition duration-700  `}>
 
 
