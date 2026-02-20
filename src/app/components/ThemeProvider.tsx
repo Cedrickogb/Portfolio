983d8bdafcb9898df23c7d4b79039c6db2177e69
Cedrickogb <cedrickogoubiyi13@gmail.com>
2026-02-20 01:12:25 +0100

    revu du themeProvider

--- src/app/components/ThemeProvider.tsx
+++ src/app/components/ThemeProvider.tsx
@@ -20,15 +20,15 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
   };
 
   return (
-    <div className="min-h-screen flex flex-col font-sans bg-background-light dark:bg-background-dark relative transition-colors duration-300  selection:bg-primary selection:text-black overflow-x-hidden scrollBar">
+    <main className="min-h-screen flex flex-col font-sans bg-background-light dark:bg-background-dark relative transition-colors duration-300  selection:bg-primary selection:text-black overflow-x-hidden scrollBar">
       <div className="scanline-overlay pointer-events-none fixed inset-0 z-50"></div>
       <div className="fixed inset-0 z-0 pointer-events-none">
         <div className="absolute inset-0 bg-grid-pattern-light dark:bg-grid-pattern bg-[length:40px_40px] opacity-[0.05]"></div>
       </div>
 
       <Navbar theme={theme} toggleTheme={toggleTheme} />
-         <main className="flex-grow z-10">{children}</main>
+      <section className="flex-grow z-10">{children}</section>
       <Footer />
-    </div>
+    </main>
   );
 }
