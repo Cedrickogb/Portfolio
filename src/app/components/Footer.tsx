32f9ac270e3188905926088cf3c35780084358ca
Cedrickogb <cedrickogoubiyi13@gmail.com>
2026-02-20 01:09:40 +0100

    revu du footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full py-6 text-center bg-gray-100 dark:bg-black transition-colors duration-300 border-t border-gray-200 dark:border-gray-800">
-      <p className="font-mono text-gray-600 dark:text-gray-500 text-xs transition-colors">
-        Copyright © 2020 - 2024. Cédrick OGOUBIYI
+    <footer className="w-full py-5 text-center bg-gray-100 dark:bg-black transition-colors duration-300 border-t border-gray-200 dark:border-gray-800">
+      <p className="font-mono text-gray-600 dark:text-gray-400 text-sm transition-colors">
+        Copyright © 2020 - 2026. Cédrick OGOUBIYI
       </p>
     </footer>
   );
